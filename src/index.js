502b96eada3c541e81dae0ead8ddc732fffd1143
sage-church <sagin.churchman77@gmail.com>
2022-02-18 21:19:46 -0600

    progress on validating color change with multiple columns

--- src/index.js
+++ src/index.js
@@ -9,16 +9,29 @@ class Board extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      circles: Array.from({length: 6}, () => ({backgroundColor: 'rgb(194, 194, 194)'})),
+      circles: Array.from({length: 12}, () => ({backgroundColor: 'rgb(194, 194, 194)'})),
       redIsNext: true
     }
     
   }
 
   handleClick (i) {
+
     const circles = this.state.circles.slice();
-    var redIsNext = this.state.redIsNext
+    const columns = [
+      [0, 1, 2, 3, 4, 5],
+      [6, 7, 8, 9, 10, 11]
+    ];
+    var redIsNext = this.state.redIsNext;
     var changeColor = false;
+
+    for (let y = 0; y < columns.length; y++) {
+      for (let j = 0; j < columns[j].length; j++) {
+        if (i = columns[y][j]) {
+          
+        }
+      }
+    }
 
     if (i === 0 && 
       circles[i].backgroundColor !== 'red' && 
@@ -50,44 +63,82 @@ class Board extends React.Component {
   render () {
     return (
       <div className='entire-board'>
-        <div className='column'>
-          <div 
-            className='circle'
-            style={{backgroundColor: this.state.circles[5].backgroundColor}}
-            onClick={() => this.handleClick(5)} 
-          >
+          <div className='column'>
+              <div 
+                className='circle'
+                style={{backgroundColor: this.state.circles[5].backgroundColor}}
+                onClick={() => this.handleClick(5)} 
+              >
+              </div>
+              <div 
+                className='circle'
+                style={{backgroundColor: this.state.circles[4].backgroundColor}}
+                onClick={() => this.handleClick(4)} 
+              >
+              </div>
+              <div 
+                className='circle'
+                style={{backgroundColor: this.state.circles[3].backgroundColor}}
+                onClick={() => this.handleClick(3)} 
+              >
+              </div>
+              <div 
+                className='circle'
+                style={{backgroundColor: this.state.circles[2].backgroundColor}}
+                onClick={() => this.handleClick(2)} 
+              >
+              </div>
+              <div 
+                className='circle'
+                style={{backgroundColor: this.state.circles[1].backgroundColor}}
+                onClick={() => this.handleClick(1)} 
+              >
+              </div>
+              <div 
+                className='circle'
+                style={{backgroundColor: this.state.circles[0].backgroundColor}}
+                onClick={() => this.handleClick(0)} 
+              >
+              </div>
           </div>
-          <div 
-            className='circle'
-            style={{backgroundColor: this.state.circles[4].backgroundColor}}
-            onClick={() => this.handleClick(4)} 
-          >
+          <div className='column'>
+              <div 
+                className='circle'
+                style={{backgroundColor: this.state.circles[11].backgroundColor}}
+                onClick={() => this.handleClick(11)} 
+              >
+              </div>
+              <div 
+                className='circle'
+                style={{backgroundColor: this.state.circles[10].backgroundColor}}
+                onClick={() => this.handleClick(10)} 
+              >
+              </div>
+              <div 
+                className='circle'
+                style={{backgroundColor: this.state.circles[9].backgroundColor}}
+                onClick={() => this.handleClick(9)} 
+              >
+              </div>
+              <div 
+                className='circle'
+                style={{backgroundColor: this.state.circles[8].backgroundColor}}
+                onClick={() => this.handleClick(8)} 
+              >
+              </div>
+              <div 
+                className='circle'
+                style={{backgroundColor: this.state.circles[7].backgroundColor}}
+                onClick={() => this.handleClick(7)} 
+              >
+              </div>
+              <div 
+                className='circle'
+                style={{backgroundColor: this.state.circles[6].backgroundColor}}
+                onClick={() => this.handleClick(6)} 
+              >
+              </div>
           </div>
-          <div 
-            className='circle'
-            style={{backgroundColor: this.state.circles[3].backgroundColor}}
-            onClick={() => this.handleClick(3)} 
-          >
-          </div>
-          <div 
-            className='circle'
-            style={{backgroundColor: this.state.circles[2].backgroundColor}}
-            onClick={() => this.handleClick(2)} 
-          >
-          </div>
-          <div 
-            className='circle'
-            style={{backgroundColor: this.state.circles[1].backgroundColor}}
-            onClick={() => this.handleClick(1)} 
-          >
-          </div>
-          <div 
-            className='circle'
-            style={{backgroundColor: this.state.circles[0].backgroundColor}}
-            onClick={() => this.handleClick(0)} 
-          >
-          </div>
-        </div>
       </div>
     );
   }

bcdd9d1c539e63b61b03646d40b09bec249bc111
sage-church <sagin.churchman77@gmail.com>
2022-02-18 18:43:12 -0600

    First commit. One column of clickable circles that follow game rules.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,90 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+
+class Board extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      circles: Array.from({length: 6}, () => ({backgroundColor: 'rgb(194, 194, 194)'})),
+    }
+    
+  }
+
+  handleClick (i) {
+    const circles = this.state.circles.slice();
+    var changeToRed = false;
+
+    if (i === 0 && circles[i].backgroundColor !== 'red') {
+      circles[i].backgroundColor = 'red'
+    } else {
+        for (let x = 0; x < i; x++) {
+          if (circles[x].backgroundColor === 'rgb(194, 194, 194)') {
+            break;
+          } else if (x === i - 1){
+            changeToRed = true;
+          }
+        }
+        if (changeToRed) {
+          circles[i].backgroundColor = 'red'
+        }
+    } 
+      this.setState ({circles: circles}); 
+  }
+
+
+  render () {
+    return (
+      <div className='entire-board'>
+        <div className='column'>
+          <div 
+            className='circle'
+            style={{backgroundColor: this.state.circles[5].backgroundColor}}
+            onClick={() => this.handleClick(5)} 
+          >
+          </div>
+          <div 
+            className='circle'
+            style={{backgroundColor: this.state.circles[4].backgroundColor}}
+            onClick={() => this.handleClick(4)} 
+          >
+          </div>
+          <div 
+            className='circle'
+            style={{backgroundColor: this.state.circles[3].backgroundColor}}
+            onClick={() => this.handleClick(3)} 
+          >
+          </div>
+          <div 
+            className='circle'
+            style={{backgroundColor: this.state.circles[2].backgroundColor}}
+            onClick={() => this.handleClick(2)} 
+          >
+          </div>
+          <div 
+            className='circle'
+            style={{backgroundColor: this.state.circles[1].backgroundColor}}
+            onClick={() => this.handleClick(1)} 
+          >
+          </div>
+          <div 
+            className='circle'
+            style={{backgroundColor: this.state.circles[0].backgroundColor}}
+            onClick={() => this.handleClick(0)} 
+          >
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Board />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
